2ec1c3c81ce60d6d228fdac9670bdf0073c02f39
mrvalraven <akatiago@gmail.com>
2021-12-11 23:01:19 +0000

    init

---
+++ index.mjs
@@ -0,0 +1,39 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
+const habitablePlanets = [];
+const fileName = "kepler_data.csv";
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+fs.createReadStream(fileName)
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(
+      habitablePlanets.map((planet) => {
+        return planet["kepler_name"];
+      })
+    );
+    console.log(habitablePlanets.length + " habitable planets found");
+    console.log("done");
+  });
